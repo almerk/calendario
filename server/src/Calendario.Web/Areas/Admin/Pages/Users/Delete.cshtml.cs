3c4f5bcff7a112504ccdf2d0898ad7d7c9df0212
almerk <almerk.work@gmail.com>
2021-06-01 17:52:43 +0300

    Added delete page

---
+++ server/src/Calendario.Web/Areas/Admin/Pages/Users/Delete.cshtml.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Calendario.Core.Subjects;
+using Calendario.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Calendario.Web.Areas.Admin.Pages.Users
+{
+    public class DeleteModel : PageModel
+    {
+
+        private readonly IRepository _repository;
+
+        public DeleteModel(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public string Message { get; set; }
+        public async Task<IActionResult> OnGetAsync([Required] string id)
+        {
+            var user = await _repository.GetByIdAsync<User>(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            Message = $"Are you sure you want to delete user {user.Login}?";
+            return Page();
+        }
+        public async Task<IActionResult> OnPostAsync([Required] string id)
+        {
+            var user = await _repository.GetByIdAsync<User>(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            await _repository.DeleteAsync(user);
+            return RedirectToPage("./Index");
+        }
+    }
+}
